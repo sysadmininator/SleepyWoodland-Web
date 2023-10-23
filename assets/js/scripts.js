b6b4b3d3a22361aff71674ec55e6323f85e0c32c
Andrew Reeves <sysadmininator@users.noreply.github.com>
2023-10-23 16:01:28 -0700

    fades

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,10 +4,15 @@ let isNight = false;
 document.getElementById('character').addEventListener('click', toggleSleep);
 
 function toggleSleep() {
-    isAsleep = !isAsleep;
     const character = document.getElementById('character');
-    character.src = isAsleep ? 'assets/img/fox_asleep.png' : 'assets/img/fox_awake.png';
-    toggleDayNight();
+    character.style.opacity = 0;  // Set opacity to 0 to start fade out
+    
+    setTimeout(() => {
+        isAsleep = !isAsleep;
+        character.src = isAsleep ? 'assets/img/fox_asleep.png' : 'assets/img/fox_awake.png';
+        character.style.opacity = 1;  // Set opacity to 1 to start fade in
+        toggleDayNight();
+    }, 1000);  // Wait for 1 second (the duration of the fade out) before switching the image and starting the fade in
 }
 
 function toggleDayNight() {
